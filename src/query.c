1f9c1123a7d33f5185991e4651a7f9cd9cf8cb17
Seppe Degryse <80254822+Griezn@users.noreply.github.com>
2024-12-08 19:58:07 +0100

    Stricter parameter types

--- src/query.c
+++ src/query.c
@@ -16,7 +16,7 @@
 /// @param in2 The second input stream to be joined
 /// @param out The output stream containing matching from the first and hte second stream
 /// @param param Join parameters containing a function ptr specifying the join condition
-void join(const data_t *in1, const data_t *in2, data_t *out, const parameter_t param)
+void join(const data_t *in1, const data_t *in2, data_t *out, const join_params_t param)
 {
     const uint32_t size = (in1->size * in2->size) * (in1->width + in2->width);
     out->data = malloc(size * sizeof(triple_t));
@@ -26,7 +26,7 @@ void join(const data_t *in1, const data_t *in2, data_t *out, const parameter_t p
 
     for (uint32_t i = 0; i < in1->size*in2->width; i += in1->width) {
         for (uint32_t j = 0; j < in2->size * in2->width; j += in2->width) {
-            if (join_check(in1, i, in2, j, param.join)) {
+            if (join_check(in1, i, in2, j, param)) {
                 join_triple_copy(in1, i, in2, j, out);
             }
         }
@@ -39,7 +39,7 @@ void join(const data_t *in1, const data_t *in2, data_t *out, const parameter_t p
 /// @param out The filtered output stream
 /// @param param The filter parameters containing a function ptr specifying the filter condition
 /// @note The out buffer will likely be larger than the out->size
-void filter(const data_t *in, data_t *out, const parameter_t param)
+void filter(const data_t *in, data_t *out, const filter_params_t param)
 {
     const uint32_t size = in->size * in->width;
     out->data = malloc(size * sizeof(triple_t));
@@ -48,7 +48,7 @@ void filter(const data_t *in, data_t *out, const parameter_t param)
     out->width = in->width;
 
     for (uint32_t i = 0; i < size; i += in->width) {
-        if (filter_check(in, i, param.filter)) {
+        if (filter_check(in, i, param)) {
             triple_copy(in, i, out);
         }
     }
@@ -59,9 +59,9 @@ void filter(const data_t *in, data_t *out, const parameter_t param)
 /// @param in The input stream
 /// @param out A selection of the input stream
 /// @param param The window parameter containing a size of the window
-void window(const data_t *in, data_t *out, const parameter_t param)
+void window(const data_t *in, data_t *out, const uint32_t param)
 {
-    const uint32_t size = min(in->size, param.window) * in->width;
+    const uint32_t size = min(in->size, param) * in->width;
     out->data = malloc(size  * sizeof(triple_t));
     assert(out->data);
     out->size = in->size;
@@ -77,17 +77,17 @@ void window(const data_t *in, data_t *out, const parameter_t param)
 /// @param in The input stream
 /// @param out The input stream with only the triples having one of the specified predicates
 /// @param param The select parameter containing an array with the wanted predicates
-void select_query(const data_t *in, data_t *out, const parameter_t param)
+void select_query(const data_t *in, data_t *out, const select_params_t param)
 {
-    const uint32_t size = in->size * param.select.size;
+    const uint32_t size = in->size * param.size;
     out->data = malloc(size * sizeof(triple_t));
     out->size = in->size;
-    out->width = param.select.size;
+    out->width = param.size;
     uint32_t out_idx = 0;
 
     for (uint32_t i = 0; i < in->size; ++i) {
         for (uint32_t j = 0; j < in->width; ++j) {
-            if (select_check(in, i * in->width + j, param.select)) {
+            if (select_check(in, i * in->width + j, param)) {
                 out->data[out_idx++] = in->data[i * in->width + j];
             }
         }
@@ -104,22 +104,22 @@ void *execute_operator_thread(void *arg) {
 
 
 /// This function executed the right operator
-/// @param operator_ The operator to be executed
+/// @param operator The operator to be executed
 /// @param in The input stream
 /// @param out The output stream
-void execute_operator(const operator_t *operator_, const data_t *in, data_t *out)
+void execute_operator(const operator_t *operator, const data_t *in, data_t *out)
 {
     data_t tmpo1 = *in;
     data_t tmpo2 = {NULL, 0, 1};
 
-    switch (operator_->type) {
+    switch (operator->type) {
         case JOIN:
-            assert(operator_->left);
-            assert(operator_->right);
+            assert(operator->left);
+            assert(operator->right);
 
             // Thread arguments
-            operator_thread_arg_t left_arg = {operator_->left, in, &tmpo1};
-            operator_thread_arg_t right_arg = {operator_->right, in, &tmpo2};
+            operator_thread_arg_t left_arg = {operator->left, in, &tmpo1};
+            operator_thread_arg_t right_arg = {operator->right, in, &tmpo2};
 
             // Threads
             pthread_t left_thread, right_thread;
@@ -132,30 +132,30 @@ void execute_operator(const operator_t *operator_, const data_t *in, data_t *out
             pthread_join(left_thread, NULL);
             pthread_join(right_thread, NULL);
 
-            join(&tmpo1, &tmpo2, out, operator_->params);
+            join(&tmpo1, &tmpo2, out, operator->params.join);
 
             free(tmpo2.data);
             tmpo2.data = NULL;
             break;
         case FILTER:
-            if (operator_->left) {
-                execute_operator(operator_->left, in, &tmpo1);
+            if (operator->left) {
+                execute_operator(operator->left, in, &tmpo1);
             }
 
-            filter(&tmpo1, out, operator_->params);
+            filter(&tmpo1, out, operator->params.filter);
             break;
         case WINDOW:
-            if (operator_->left) {
-                execute_operator(operator_->left, in, &tmpo1);
+            if (operator->left) {
+                execute_operator(operator->left, in, &tmpo1);
             }
 
-            window(&tmpo1, out, operator_->params);
+            window(&tmpo1, out, operator->params.window);
             break;
         case SELECT:
-            if (operator_->left)
-                execute_operator(operator_->left, in, &tmpo1);
+            if (operator->left)
+                execute_operator(operator->left, in, &tmpo1);
 
-            select_query(&tmpo1, out, operator_->params);
+            select_query(&tmpo1, out, operator->params.select);
             break;
     }
 
